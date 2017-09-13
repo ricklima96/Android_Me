dce9b53c0f96e6532a3e8c77687826963b683c59
Taqtile <taqtile@GreenCow.local>
2017-09-13 11:26:07 -0300

    Corrigindo erros no código

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -21,12 +21,6 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class AndroidMeActivity extends AppCompatActivity {
 
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,46 +60,6 @@ public class AndroidMeActivity extends AppCompatActivity {
                     .add(R.id.legs_container, legsFragment)
                     .commit();
         }
-
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("AndroidMe Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }
--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.android_me.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,6 +12,10 @@ import android.widget.Toast;
 
 import com.example.android.android_me.R;
 import com.example.android.android_me.data.AndroidImageAssets;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 // This activity is responsible for displaying the master list of all images
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener {
@@ -22,15 +27,17 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
     private boolean mTwoPane;
 
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        if(findViewById(R.id.android_me_layout) != null){
+        if (findViewById(R.id.android_me_linear_layout) != null) {
             mTwoPane = true;
-            Button nextButton = (Button) findViewById(R.id.nex_button);
+            Button nextButton = (Button) findViewById(R.id.next_button);
             nextButton.setVisibility(View.GONE);
 
             GridView gridView = (GridView) findViewById(R.id.images_grid_view);
@@ -73,21 +80,24 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
             }
 
 
-        }else{
+        } else {
             mTwoPane = false;
         }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
     public void onImageSelected(int position) {
-        Toast.makeText(this, "Posicao: "+ position, Toast.LENGTH_SHORT ).show();
+        Toast.makeText(this, "Posicao: " + position, Toast.LENGTH_SHORT).show();
 
         int bodyPartNumber = position / 12;
 
-        int listIndex = position - 12*bodyPartNumber;
+        int listIndex = position - 12 * bodyPartNumber;
 
-        if(mTwoPane) {
+        if (mTwoPane) {
             BodyPartFragments newFragment = new BodyPartFragments();
 
             switch (bodyPartNumber) {
@@ -107,8 +117,7 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
                     getSupportFragmentManager().beginTransaction().replace(R.id.legs_container, newFragment).commit();
                     break;
             }
-        }else{
-            }
+        } else {
             switch (bodyPartNumber) {
                 case 0:
                     headIndex = listIndex;
@@ -125,10 +134,11 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
             }
         }
 
+
         Bundle b = new Bundle();
-        b.putInt("headIndex", headIndex);
-        b.putInt("bodyIndex", bodyIndex);
-        b.putInt("legIndex", legIndex);
+        b.putInt("headIndex",headIndex);
+        b.putInt("bodyIndex",bodyIndex);
+        b.putInt("legIndex",legIndex);
 
         // Attach the Bundle to an intent
         final Intent intent = new Intent(this, AndroidMeActivity.class);
@@ -136,14 +146,49 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
 
         // The "Next" button launches a new AndroidMeActivity
         Button nextButton = (Button) findViewById(R.id.next_button);
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        nextButton.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick (View view){
                 startActivity(intent);
             }
         });
+    }
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
